dab3a573da0c6adeffe4041096ded429e1cf9af0
rohityadav-downloader <rohityadav.sas@gmail.com>
2025-08-28 22:55:27 +0545

    Update types.d.ts

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -11,3 +11,26 @@ interface TelegramMessage {
 interface TelegramUpdate {
 	message: TelegramMessage
 }
+
+interface InstagramDownloadUrl {
+	url: string
+	name: string
+	type: string
+	ext: string
+}
+
+interface InstagramApiResponse {
+	url: InstagramDownloadUrl[]
+	meta: {
+		title: string
+		source: string
+		shortcode: string
+		like_count: number
+		comment_count: number
+		taken_at: number
+		username: string
+	}
+	thumb: string
+	hosting: string
+	timestamp: number
+}
